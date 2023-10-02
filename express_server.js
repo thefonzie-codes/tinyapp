72eb9aadbab405f1c6312df7da3d071ba6c65ab6
Alfonso <alfonsobanzon@gmail.com>
2023-10-02 18:10:31 -0400

    added headers, urls_new.js has a form to input long urls

--- express_server.js
+++ express_server.js
@@ -2,7 +2,11 @@ const express = require('express');
 const app =  express();
 const PORT = 8080; // default port 8080
 
+const generateRandomString = () => {Math.random().toString(36).slice(2, 8)};
+
 app.set('view engine', 'ejs');
+
+app.use(express.urlencoded({ extended: true }));
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
@@ -13,14 +17,19 @@ app.get('/', (req, res) => { // app.get will display a page based on the path. "
   res.send("Hello!");
 });
 
-app.get('/hello', (req, res) => {
-  res.send("<html><body>Hello <b>World</b></body></html>\n");
-});
-
 app.get('/urls', (req, res) => {
   const templateVars = { urls: urlDatabase };
   res.render("urls_index"/*file name in views folder*/, templateVars);
 });
+
+app.post('/urls', (req, res) => {
+  console.log(req.body);
+  res.send("ok");
+})
+
+app.get("/urls/new", (req,res) => { 
+  res.render("urls_new")
+})
 
 app.get("/urls/:id", (req,res) => {
   const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id] };
