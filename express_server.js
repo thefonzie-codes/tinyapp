02a09c277a2e6de444d02754f57bf0814ff0d1cb
Alfonso <alfonsobanzon@gmail.com>
2023-10-04 18:17:16 -0400

    cleaned up login and registration functionality

--- express_server.js
+++ express_server.js
@@ -2,9 +2,7 @@ const express = require('express');
 const app =  express();
 const PORT = 8080; // default port 8080
 const cookieParser = require('cookie-parser');
-const { generateRandomString } = require('./helpers.js');
-const { createNewUser } = require('./helpers.js')
-const { returnIdFromEmail } = require('./helpers.js')
+const { generateRandomString, createNewUser, returnIdFromEmail, login } = require('./helpers.js');
 const { users } = require('./users.js')
 
 app.use(cookieParser());
@@ -28,9 +26,10 @@ app.get('/', (req, res) => { // app.get will display a page based on the path. '
 app.get('/urls', (req, res) => {
   const templateVars = { 
     urls: urlDatabase,
-    userId: req.cookies.id,
+    userId: req.cookies.user_id,
     users,
   };
+  console.log(templateVars.userId)
   res.render('urls_index'/*file name in views folder*/, templateVars);
 });
 
@@ -83,21 +82,11 @@ app.post('/urls/:id/', (req, res) => {
   res.redirect('/urls')
 })
 
-// creates a cookie
-
-app.post('/login', (req, res) => {
-  const { email } = req.body;
-  const id = returnIdFromEmail(users, email);
-  console.log(id);
-  res.cookie('id', users[id]);
-  res.redirect('/urls')
-;})
-
 // logs user out and clears created cookie
 
 app.post('/logout', (req, res) => {
-  res.clearCookie('username')
-  res.redirect('/urls')
+  res.clearCookie('user_id')
+  res.redirect('/login')
 })
 
 // User registration functionality
@@ -110,34 +99,49 @@ app.get('/register', (req, res) => {
 
 app.post('/register', (req, res) => {
   
-  if (!req.body.email) {
-    res.statusMessage ='E-mail required';
-    res.send(400, '400 Error: email required');
-  };
-  
-  if (!req.body.password) {
-    res.statusMessage ='password required';
-    res.send(400, '400 error: password required');
-  };
-  
-  const newUser = createNewUser(req.body);
-  const userId = newUser.id;
-  users[userId] = newUser.newUser;
+  const newUser = createNewUser(users, req.body);
+
+  if (newUser.errStatus) {
+    res.send(newUser.errMsg, newUser.errStatus)
+  }
+
+  const user_id = newUser.id;
+  users[user_id] = newUser.newUser;
   console.log(users);
-  res.cookie('id', userId)
+  res.cookie('user_id', user_id)
   res.redirect('/urls');
 })
 
 //Login Form
 
 app.get('/login', (req, res) => {
-  templateVars = { users, userId: ""}
+  templateVars = { 
+    id: req.params.id, 
+    urlDatabase: urlDatabase,   
+    userId: req.cookies.user_id,
+    users
+  }
+  console.log(users);
   res.render('login', templateVars)
 })
 
+app.post('/login', (req, res) => {
+  const userId = login(users, req.body);
+  console.log(userId.id)
+  
+  if (userId.errStatus) {
+    res.send(userId.errStatus, userId.errMsg)
+  }
+
+  res.cookie('user_id', userId.id);
+  res.redirect('/urls');
+;})
+
+/////
+
 app.get('/u/:id', (req,res) => {
   const templateVars = {
-    userId: req.cookies.id,
+    userId: req.cookies.user_id,
     users,
     longURL: urlDatabase[req.params.id]
     // if (!longURL.startsWith('http')) {
--- helpers.js
+++ helpers.js
@@ -1,21 +1,83 @@
-const generateRandomString = () => Math.random().toString(36).slice(2, 8);
+// TEST OBJ
 
-const createNewUser = (input) => {
-  const id = generateRandomString();
-  newUser = input[id] = {
-    id,
-    ...input
+const users = {
+  userRandomID: {
+    id: "userRandomID",
+    email: "user@example.com",
+    password: "purple-monkey-dinosaur",
   }
-
-  return { id: id, newUser }
 };
 
-const returnIdFromEmail = (users, email) => {
-  for (let id of users) {
-    if (id.email === email){
-      return id;
+//
+
+const generateRandomString = () => Math.random().toString(36).slice(2, 8);
+
+const returnIdFromEmail = (obj, email) => {
+  
+  for (let id in obj) {
+    if (obj[id].email === email){
+      return obj[id].id;
     }
   }
 }
 
-module.exports = { generateRandomString, createNewUser, returnIdFromEmail }
+const createNewUser = (obj, input) => {
+
+  const inputEmail = input.email;
+  const inputPassword = input.password;
+
+
+  if (!inputEmail) {
+    return { errStatus: 403, errMsg: 'Email required', id:{}, newUser:{} }
+  };
+  
+  if (!inputPassword) {
+    return { errStatus: 403, errMsg: 'Password Required', id:{}, newUser:{} }
+  };
+
+  for (let id in obj) {
+    const existingEmail = obj[id].email;
+    console.log(inputEmail)
+    if (existingEmail === inputEmail) {
+    return { errStatus: 403, errMsg: 'Email already in use', id:{}, newUser:{} }
+    }
+  }
+
+  const newId = generateRandomString();
+  newUser = input[newId] = {
+    newId,
+    ...input
+  }
+
+  return { errStatus: null, errMsg: null, id: newId, newUser }
+};
+
+
+
+
+const login = (obj, input) => {
+
+  const id = returnIdFromEmail(obj, input.email);
+  
+  if (!id) {
+    return { errStatus: 403, errMsg: 'Email does not match any current user', id}
+  }
+
+  const userPassword = obj[id].password;
+  const inputPassword = input.password;
+
+  if (!inputPassword) {
+    return { errStatus: 403, errMsg: 'Password required', id}
+  }
+
+  if (userPassword !== inputPassword) {
+    return { errStatus: 403, errMsg: 'Username/password incorrect', id}
+  }
+
+  return { errStatus: null, errMsg: null, id }
+}
+
+console.log(createNewUser(users, {email: "123@example.com",
+password: "purple-monkey-dinosaur"}));
+
+module.exports = { generateRandomString, createNewUser, returnIdFromEmail, login }
